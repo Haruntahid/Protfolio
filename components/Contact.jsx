c4cfcd2b3909a504ac54bafd1ce898ab611f76f7
haruntahid <tahidtaha997@gmail.com>
2024-07-04 04:50:16 +0600

    added footer

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -14,10 +14,6 @@ function Contact() {
       <h2 className="text-5xl font-semibold mb-12 text-center">Contact Me</h2>
       <div className="flex justify-between items-center bg-[#3d3e42] rounded-lg p-10">
         <div>
-          <h2 className="text-4xl font-semibold text-gold-color">
-            Harun Tahid
-          </h2>
-          <p className="text-xl tracking-wider">Junior Web Developer</p>
           <div className="flex items-center gap-2 bg-bg-clr px-5 py-2 rounded-full mt-5">
             <SiGmail />
             <p className="text-gold-color">tahidtaha997@gmail.com</p>
@@ -31,6 +27,19 @@ function Contact() {
             <p className="text-gold-color">Dinajpur,Bangladesh</p>
           </div>
         </div>
+        <div className="text-center space-y-3">
+          <h2 className="text-5xl font-semibold text-gold-color">
+            Harun Tahid
+          </h2>
+          <p className="text-2xl tracking-wider">Junior Web Developer</p>
+          <div>
+            <button className="px-12 mt-5 py-2 text-black text-xl bg-gold-color rounded-full">
+              Resume
+            </button>
+          </div>
+        </div>
+        {/* middle section */}
+
         <div className="flex gap-4">
           <a href="https://www.facebook.com/Haruntaha997/" target="_blank">
             <Image
---
+++ components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="container mx-auto mt-20">
+      <div className="block items-center w-5/6 mx-auto h-[2px] bg-gold-color"></div>
+      <h2 className="text-gold-color text-3xl font-semibold text-center mt-10">
+        {"<haruntahid />"}
+      </h2>
+      <div>
+        <ul className="flex justify-center mt-4 mb-6 text-xl space-x-14 text-txt-clr">
+          <li>Skills</li>
+          <li>Projects</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+      <p className="text-center text-txt-white pb-8">
+        @copyright 2024 <span>haruntahid</span>. All Rights Reserved.
+      </p>
+    </div>
+  );
+}
+
+export default Footer;
