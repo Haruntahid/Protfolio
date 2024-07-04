dcb6901c19e31f4bfe6037bbf63170df53362d7c
haruntahid <tahidtaha997@gmail.com>
2024-07-05 02:10:18 +0600

    add: projects links

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,3 +1,4 @@
+"use client";
 import Image from "next/image";
 import React from "react";
 import needBlood from "../images/projects/Need Blood.png";
@@ -6,50 +7,59 @@ import bookStore from "../images/projects/Vix-BookStore.png";
 import ticket from "../images/projects/Vix-ticket.png";
 import villa from "../images/projects/vix-villa Home.png";
 import vixCareer from "../images/projects/Vix-Career Home.png";
+import { FaGithub } from "react-icons/fa6";
+import { BiWorld } from "react-icons/bi";
+import Link from "next/link";
 
 function Projects() {
   const projects = [
     {
       id: 1,
       name: "Need Blood",
-      slug: "",
+      slug: "Online Blood Donation Platform",
       image: needBlood,
       link: "https://need-blood-8367e.web.app",
+      code: "https://github.com/Haruntahid/need-blood-client.git",
     },
     {
       id: 2,
       name: "Vix Career",
-      slug: "",
+      slug: "Job Finding Platform For Developers",
       image: vixCareer,
       link: "https://vix-career.web.app/",
+      code: "https://github.com/Haruntahid/vix-career-client.git",
     },
     {
       id: 3,
       name: "Vix Restaurant",
-      slug: "",
+      slug: "Online Food Ordering and Reservation",
       image: restutrant,
       link: "https://vix-restaurant.netlify.app/",
+      code: "https://github.com/Haruntahid/vix-restaurant.git",
     },
     {
       id: 4,
       name: "Vix BookStore",
-      slug: "",
+      slug: "Online Book Shopping Platform",
       image: bookStore,
       link: "https://vix-bookstore.netlify.app",
+      code: "https://github.com/Haruntahid/vix-bookStore.git",
     },
     {
       id: 5,
       name: "Vix Ticket",
-      slug: "",
+      slug: "Online Ticket Booking Platform",
       image: ticket,
       link: "https://vix-ticket.netlify.app",
+      code: "https://github.com/Haruntahid/vix-ticket.git",
     },
     {
       id: 6,
       name: "Vix Villa",
-      slug: "",
+      slug: "Vacation Rental Booking Platform",
       image: villa,
       link: "https://vix-villa.netlify.app",
+      code: "https://github.com/Haruntahid/vix-villa.git",
     },
   ];
   return (
@@ -78,9 +88,26 @@ function Projects() {
                 </div>
               </div>
               <div className="mt-4">
-                <p className="text-txt-white font-bold text-xl">
+                <p className="text-txt-white font-bold text-2xl">
                   {project.name}
                 </p>
+                <p className="mt-3 text-xl text-txt-clr">{project.slug}</p>
+                <div className="mt-4 flex gap-5">
+                  <Link
+                    href={project.link}
+                    target="_blank"
+                    className="px-5 py-2 bg-transparent text-gold-color border border-gold-color  rounded-md flex items-center gap-2"
+                  >
+                    Live Site <BiWorld />
+                  </Link>
+                  <Link
+                    href={project.code}
+                    target="_blank"
+                    className="px-5 py-2 bg-transparent text-gold-color border border-gold-color  rounded-md flex items-center gap-2"
+                  >
+                    Github <FaGithub />
+                  </Link>
+                </div>
               </div>
             </div>
           ))}
